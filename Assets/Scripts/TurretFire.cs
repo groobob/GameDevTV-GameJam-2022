e1ba8dccfeb7fb411d24bc7fe1eac69b1800bf49
groobob <91916133+groobob@users.noreply.github.com>
2022-05-22 01:33:27 -0400

    implemented turret firing

---
+++ Assets/Scripts/TurretFire.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretFire : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] float firerate;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] GameObject Canvas;
+    float cooldown = 0;
+
+    void Update()
+    {
+        cooldown += Time.deltaTime;
+        if(cooldown > firerate)
+        {
+            GameObject instantiatedBullet = Instantiate(bullet, transform.position - new Vector3(0, 0, 5), Quaternion.identity);
+            instantiatedBullet.transform.parent = Canvas.transform;
+            Rigidbody2D instantiatedBulletRB = instantiatedBullet.GetComponent<Rigidbody2D>();
+            instantiatedBulletRB.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
+            Destroy(instantiatedBullet, 10f);
+            cooldown -= firerate;
+        }
+    }
+}
